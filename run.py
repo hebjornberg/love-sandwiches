b3441f5a767dc85fb28b5f05de695634f75edca8
Helena Bjornberg <helena.bjornberg@hotmail.com>
2024-04-09 17:20:17 +0000

    Add

--- run.py
+++ run.py
@@ -101,6 +101,22 @@ def get_last_5_entries():
     
     return columns
 
+def calculate_stock_data(data):
+    """
+    Calculate the average stock for each item type, adding 10%
+    """
+    print("Calculating stock data...\n")
+    new_stock_data = []
+
+    for column in data:
+        int_column = [int(num) for num in column]
+        average = sum(int_column) / len(int_column)
+        stock_num = average * 1.1
+        new_stock_data.append(round(stock_num))
+
+    return new_stock_data
+
+
 
 def main():
     """
@@ -118,5 +134,6 @@ print("Welcome to Love Sandwiches Data Automation")
 # main()
 
 sales_columns = get_last_5_entries()
+calculate_stock_data(sales_columns)
 
 
